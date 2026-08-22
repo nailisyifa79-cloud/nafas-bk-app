5d8a4ca52fc4c4f74a89cca8326a2c5d794bbdeb
nailisyifa79-cloud <nailisyifa79@guru.smk.belajar.id>
2026-08-22 18:51:51 +0700

    Update and rename deepseek_bash_20260822_6c3186.sh to index.html

--- deepseek_bash_20260822_6c3186.sh
+++
@@ -1,6 +0,0 @@
-git clone https://github.com/username/nafas-bk-app.git
-cd nafas-bk-app
-# Paste kode index.html di sini
-git add index.html
-git commit -m "Initial commit - SOSIO DIGITAL NaFaS BK"
-git push origin main
