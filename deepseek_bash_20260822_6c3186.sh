48988212b748ef82d8a8970600ad8f9c2962d507
nailisyifa79-cloud <nailisyifa79@guru.smk.belajar.id>
2026-08-22 18:41:53 +0700

    Add files via upload

---
+++ deepseek_bash_20260822_6c3186.sh
@@ -0,0 +1,6 @@
+git clone https://github.com/username/nafas-bk-app.git
+cd nafas-bk-app
+# Paste kode index.html di sini
+git add index.html
+git commit -m "Initial commit - SOSIO DIGITAL NaFaS BK"
+git push origin main
